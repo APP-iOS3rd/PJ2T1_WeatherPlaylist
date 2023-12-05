0ae98472bd308f612cff08e95c59703975b4406e
iAmSomething <st939823@gmail.com>
2023-12-05 18:06:01 +0900

    프로젝트 생성

---
+++ WeatherPlaylist/WeatherPlaylist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherPlaylist
+//
+//  Created by 김태훈 on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherPlaylist/WeatherPlaylist/WeatherPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherPlaylistApp.swift
+//  WeatherPlaylist
+//
+//  Created by 김태훈 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
