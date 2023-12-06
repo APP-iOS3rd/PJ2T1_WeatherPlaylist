264a08008358646fbec52fd2cf9dd6412914004f
iAmSomething <st939823@gmail.com>
2023-12-06 12:05:14 +0900

    폴더링하고 마이페이지뷰 만들기 1. 폴더링을 하였습니다. 2. 마이페이지뷰 파일을 만들었습니다.

---
+++ WeatherPlaylist/WeatherPlaylist/Views/MypageView/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  WeatherPlaylist
+//
+//  Created by 김태훈 on 12/6/23.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
