9710ca865da2afb4b7ef19cc63ddf7a14f2b74bb
RapidSloth <yud1230@naver.com>
2023-12-07 09:13:26 +0900

    [Feat]: SpotifyiOS master - Auth 기능 구현을 위해 라이브러리 설치 - Splash 애니메이션 코드 수정

--- WeatherPlaylist/WeatherPlaylist/Views/StartView/StartView.swift
+++ WeatherPlaylist/WeatherPlaylist/Views/StartView/StartView.swift
@@ -9,30 +9,24 @@ import SwiftUI
 
 struct StartView: View {
     
-    @State var isLoading: Bool = true
+    @State var isLoading: Bool = false
     
     var body: some View {
         
         ZStack {
-            
-            // 1) CHANGED: zIndex added
-            // 앱 화면
-            LoginView()
-            
-            // Launch Screen
             if isLoading {
-                // 2) CHANGED: transition and zIndex added
-                LaunchView().transition(.opacity).zIndex(1)
+                LoginView()
+                    .transition(.opacity)
+                    .zIndex(1)
+            } else {
+                LaunchView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                            withAnimation { isLoading.toggle() }
+                        })
+                    }
             }
-            
         }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
-                // 3) CHANGED: withAnimation added
-                withAnimation { isLoading.toggle() }
-            })
-        }
-        
     }
 }
 
