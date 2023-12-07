baec22f28bd9cee5a7770b4f3ca3675d6ef67dce
김태훈 <38745420+iAmSomething@users.noreply.github.com>
2023-12-07 16:14:09 +0900

    HTTPManager추가하기 (#14)
    1. API연결하는 플로우를 구성했어요!

--- WeatherPlaylist/WeatherPlaylist/Source/UserRequest.swift
+++ WeatherPlaylist/WeatherPlaylist/Source/UserRequest.swift
@@ -7,37 +7,79 @@
 
 import Foundation
 //MARK: - 프로토콜 작성 예시
-class UserRequest<T>: APIRequestProtocol where T: Decodable{
+enum HTTPRequest {
+    case login
+    case getUserInfo
+    case serchPlaylist(query: String)
+    case serchTracks(trackId: String)
+    case userContains
+}
+final class HTTPManager<T>: APIRequestProtocol where T: Decodable{
     typealias Response = T
-    var path: String = "me"
-    var method: HTTPMethod = .get
-    var parameters: [String: Any]? = ["key": "value"]
-    init(path: String = "me",
-         method: HTTPMethod = .get,
-         parameters: [String : Any]? = nil) {
-        self.path = path
-        self.method = method
-        self.parameters = parameters
+    var path: String
+    var method: HTTPMethod
+    var parameters: [String : Any]?
+    var apiType: HTTPRequest
+    init(apiType: HTTPRequest, parameters: [String : Any]? = nil) {
+        self.apiType = apiType
+        switch apiType {
+        case .login:
+            path = "me"
+            method = .get
+            self.parameters = parameters
+        case .getUserInfo:
+            path = ""
+            method = .get
+            self.parameters = parameters
+        case .serchPlaylist(let query):
+            path = "search"
+            method = .get
+            self.parameters = ["q": query]
+        case .serchTracks(let id):
+            path = "tracks/\(id)"
+            method = .get
+            self.parameters = parameters
+        case .userContains:
+            path = "me"
+            method = .get
+            self.parameters = parameters
+        }
     }
-    //User API에서 사용되는 case별로 enum으로 관리하면 편할듯..?
-    enum apiType {
-        case myinfo
-        case myPlayList
-    }
-    func mapResponseToModel() async -> Response?{
+    //이 함수의 존재 이유가..?
+    func fetchDataModel() async -> Response? {
         do {
            let tmp = await fetchData()
             switch tmp {
             case .success(let response) :
                 return response
             case .failure(let error) :
+//                switch error {
+//                case .urlError(_):
+//
+//                case .decodingError(_):
+//
+//                case .otherError:
+//
+//                case .httpError(_):
+//
+//                }
                 print(error.errorDescription)
                 return nil
             }
         }
     }
 }
-//MARK: - 임시모델
-struct TMP: Decodable{
+//MARK: - 이런식
+struct UserData: Decodable{
     let id: UUID
+}
+func test() async {
+    let manager = HTTPManager<UserData>(apiType: .serchPlaylist(query: "happy"))
+    let result = await manager.fetchData()
+    switch result {
+    case .success(let res):
+        print("\(res.id)")
+    case .failure(_):
+        print("실패")
+    }
 }
