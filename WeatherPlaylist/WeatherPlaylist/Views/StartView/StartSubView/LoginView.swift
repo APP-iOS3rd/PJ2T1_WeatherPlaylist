33efe8ca3b809cf8b4eb193a842a9d79e325b4f1
Sungyeop Kim <144765545+RapidSloth@users.noreply.github.com>
2023-12-07 10:43:50 +0900

    [Feat]: StartView 구현 (#10)
    * [feat]: StartView 구현
    - LaunchView -> LoginView 애니메이션 기능 추가
    
    * [Feat]: SpotifyiOS master
    - Auth 기능 구현을 위해 라이브러리 설치
    - Splash 애니메이션 코드 수정
    
    * [hotfix]: 충돌수정

---
+++ WeatherPlaylist/WeatherPlaylist/Views/StartView/StartSubView/LoginView.swift
@@ -0,0 +1,89 @@
+//
+//  LoginView.swift
+//  WeatherPlaylist
+//
+//  Created by 김성엽 on 12/6/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { reader in
+                ZStack {
+                    
+                    Image("background")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: reader.size.width)
+                        .ignoresSafeArea()
+                    
+                    
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient( colors: [
+                                    .clear,
+                                    .colorBlack
+                                    ]
+                                ),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                    
+                    
+                    VStack(spacing: 20) {
+                        Spacer()
+                        Image("spotify")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:50)
+                        
+                        
+                        Text("내 마음에 꼭 드는 또 다른 플레이리스트를 발견해보세요.")
+                            .padding(.horizontal, 15)
+                            .font(.custom(FontType.Bold.rawValue, size: 40))
+                            .foregroundStyle(.white)
+                        
+                        
+                        Spacer()
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            HStack {
+                                Text("스포티파이로 시작하기")
+                                    .font(.custom(FontType.SemiBold.rawValue, size: 20))
+                                    .foregroundStyle(.black)
+                            }
+                        }
+                        .buttonStyle(signUptButton())
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+struct signUptButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical, 20)
+            .padding(.horizontal, 70)
+            .background(.colorGreen)
+            .cornerRadius(40)
+    }
+}
+
+
+
+#Preview {
+    LoginView()
+}
