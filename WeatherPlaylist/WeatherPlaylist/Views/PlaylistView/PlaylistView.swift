0840f27f07d72188bd3933a84d62eb1d95959809
xohxe <saeyan2477@gmail.com>
2023-12-11 02:28:29 +0900

    [Feat]: HeaderView 기능 수정 및 화면전환 위한 PlayListView 수정
    - Modal Sheet로 화면 전환(isPresented)시, 커스텀된 HeaderView를 적용하였습니다.
      - 음악 재생 화면만 모달 뷰를 적용하기 위해, 커스텀한 HeaderView를 일부 수정하였습니다.
      - 하단 미니플레이어로 재생화면을 이동할 때, 재생리스트 화면에서 곡 클릭 시 모달로 띄워지게 적용했습니다.
      - Modal이 아래에서 위로 펼쳐져, `<` 대신 `v` 아이콘을 변경하였습니다.
    
    - NavigationLink로 화면 전환시, 커스텀된 Headerview 적용대신 기본 네비게이션바를 적용하였습니다.
      - 마이페이지, 플레이리스트 페이지는 기본 네비게이션 바를 사용하기 위해 HeaderView를 삭제하였습니다.
      - 음악 재생화면은 modal로 전환되는 화면으로, NavigationLink를 삭제하고, onTapGesture를 추가하여 모달창이 띄워지게 변경하였습니다.
      - fullScreenCover는 모달창이 전체화면으로 띄워주게 합니다.
      - NavigationBar Title영역의 여백을 없애기 위해, NavigationStack에서 NavigationView로 변경하였습니다.
      - 재생되고 있는 노래를 띄워주기 위해, PlayFooterCell을 추가했어요.

--- WeatherPlaylist/WeatherPlaylist/Views/PlaylistView/PlaylistView.swift
+++ WeatherPlaylist/WeatherPlaylist/Views/PlaylistView/PlaylistView.swift
@@ -8,49 +8,62 @@
 import SwiftUI
 
 struct PlaylistView: View {
+    
+    @State var isLightMode: Bool = true
+    @State private var isShowingPlayer = false
+    
     let dummyData = MusicListDummyManager().list
     @State var playlistInfo: PlayListInfo = .init(playlistName: "aaaa",
                                                   playlistDescription: "aaaaaaa",
                                                   coverImageUrl: "https://image-cdn-ak.spotifycdn.com/image/ab67706c0000bebbefacbaef716e41536fab68d4",
                                                   isLikePlaylist: false)
     var body: some View {
-        NavigationStack {
-            VStack {
-                HeaderView()
-                
+        NavigationView {
+            ZStack(alignment:.bottom)  {
                 ScrollView(.vertical, showsIndicators: false) {
                     PlaylistCorverImageView(coverImageUrl: playlistInfo.coverImageUrl)
                     
                     LazyVStack(alignment: .leading, pinnedViews: [.sectionHeaders]) {
+         
                         Section {
                             ForEach(dummyData) { song in
-                                NavigationLink(destination: {
-                                    PlayMusicView(temp: song)
-                                        .navigationBarBackButtonHidden()
-                                }) {
                                     PlaylistRowView(id: song.id,
                                                     songName: song.songName,
                                                     artist: song.artist,
                                                     coverImage: song.coverImage,
                                                     songTime: song.songTime)
-                                }
-                                
+                                    .onTapGesture {
+                                        self.isShowingPlayer.toggle()
+                                    }
+                                    .fullScreenCover(isPresented: $isShowingPlayer){
+                                        PlayMusicView(temp: song)
+                                    }
                             }
                         } header: {
                             PlaylistStickyHeader(playlistInfo: playlistInfo)
-                            
-                            
                         }
+                        
+                        
                     }
                 }
                 .padding(EdgeInsets(top: 0,
                                     leading: 24,
                                     bottom: 0,
                                     trailing: 24))
+                PlayFooterCell(musicImage: "album2",
+                               isLightMode: $isLightMode)
+             
             }
+
         }
+      
+     
+      
+     
+
+        
     }
 }
-#Preview {
-    PlaylistView()
-}
+//#Preview {
+//    PlaylistView(, checkModal: <#Binding<Bool>#>)
+//}
