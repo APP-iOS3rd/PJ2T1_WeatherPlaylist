da566d8da57b5dc055bd3466cf71e5231978caf4
woogie <54401641+jeonguk29@users.noreply.github.com>
2023-12-11 17:11:06 +0900

    Feat/Weather API 관련 기초 작업 완료  (#26)
    * [Feat] 사용자 위치(위도, 경도) 가져오기 구현 완료
    
    Core Location 프레임워크를 사용하여 사용자의 위치 정보를 가져오게 구현 하였습니다.
    
    * [Feat] 사용자 위치 경도에 따른 WeatherAPI 호출 구현 완료
    
    WeatherAPI 파싱 데이터 구조를 정리 하였고
    결과 값에 따라 아이콘을 화면에 출력하는 것을 구현 하였습니다.
    
    * [Feat] 사용자 날씨 아이콘에 따른 날씨 상태 값 문자열 구분
    
    파싱이 끝나면 user의 날씨 데이터만 가져오는 로직을 실행
    feachUserWeatherData() 에서 필요한 날씨 데이터 관련 값을 setting 하는 부분을 작성하면 될 거 같습니다.
    
    getWeatherStatus를 호출하여 사용자의 날씨 아이콘에 따른 사용자 날씨 상태 문자열을 가져옵니다.
    추후 스포티파이 API 호출 키워드로 사용할 수 있으며
    MainView에서 필요한 데이터(음악 추천 title 등)에 활용할 수 있을 거 같습니다.
    
    * [Refactoring]: Weather 관련 Code Refactoring 및 코드 설명 추가
    
    - WeatherTestView 에서 Weather API 호출 후 어떤 값이 나오는지 테스트할 수 있습니다.
    
    - 기존에 구현되어 있던 Weather Logic은 아직 합치지 않았습니다.

---
+++ WeatherPlaylist/WeatherPlaylist/Source/Singleton/Weather/WeatherTestView.swift
@@ -0,0 +1,55 @@
+//
+//  WeatherTestView.swift
+//  WeatherPlaylist
+//
+//  Created by 정정욱 on 12/11/23.
+//
+import SwiftUI
+
+
+// MARK: - 날씨 관련 API TestView
+struct WeatherTestView: View {
+
+    @StateObject var weatherData = WeatherAPI.shared
+
+    var body: some View {
+        NavigationStack {
+            List{
+                ForEach(weatherData.weatherInformation, id: \.self) { result in
+                    // 사용자 위도, 경도
+                    Text("위도:\(result.coord.lat) 경도:\(result.coord.lon)").foregroundStyle(Color.black)
+                    Text(result.weather[0].icon)
+                    
+                    // 사용자 위치 날씨 상태
+                    Text(weatherData.userWatherStatus)
+                    
+                    
+                    // 사용자 위치 날씨 아이콘 
+                    AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(result.weather[0].icon)@2x.png")) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 30, height: 30) // 회고
+                    
+                }
+            }
+        }
+        .onAppear {
+           // 시뮬 환경에서 값을 가져오지 못함 임시로 일단 넣어서 테스트
+            let locationManager = LocationManager()
+            locationManager.startUpdatingLocation()
+            print("위도: \(locationManager.latitude), 경도: \(locationManager.longitude)")
+          
+            // 사용자 위도, 경도를 전달하여 API 호출
+            weatherData.feachWeatherData(lat: locationManager.latitude, lon: locationManager.longitude)
+        }
+    }
+   
+}
+
+
+
+#Preview {
+    WeatherTestView()
+}
