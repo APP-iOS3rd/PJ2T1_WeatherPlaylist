4769e995852f972a076c37cad04f0c24999673f6
RapidSloth <yud1230@naver.com>
2023-12-13 18:45:17 +0900

    [Feat]: 미니 플레이어 - 슬라이더 위치 조정 - 슬라이더 뷰 통합

--- WeatherPlaylist/WeatherPlaylist/Views/Components/PlayFooterCell/CustomSlider.swift
+++ WeatherPlaylist/WeatherPlaylist/Views/Components/PlayFooterCell/CustomSlider.swift
@@ -10,26 +10,42 @@ import SwiftUI
 struct CustomSlider: View {
     @Binding var value: Double
     private var maxValue: Double
+    @StateObject var player = PlayerManager.shared
     
     init(value: Binding<Double>, maxValue: Double) {
         self._value = value
         self.maxValue = maxValue
     }
+    
     var body: some View {
         GeometryReader{ proxy in
-            ZStack(alignment: .center){
+            ZStack(alignment: .topLeading){
                 Capsule()
                     .foregroundColor(.secondary)
-                
+                    .frame(maxHeight: 3)
                 Capsule()
                     .fill(Color("AccentColor"))
                     .frame(width: proxy.size.width * (CGFloat(value) / CGFloat(maxValue)))
                     .contentShape(.capsule)
-                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(maxHeight: 3)
                     .overlay(.white, in: Capsule().stroke(style: .init()))
+                Slider(value: $player.songTime,
+                       in: 0...30,
+                       step: 1,
+                       onEditingChanged: { data in
+                    if data == true {
+                        player.pause()
+                        let value = player.songTime
+                        player.changeTime(time: value)
+                    } else {
+                        let value = player.songTime
+                        player.changeTime(time: value)
+                        player.play()
+                    }
+                })
+                .hidden()
             }
         }
     }
 }
-
-
